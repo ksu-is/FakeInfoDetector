7220c39e037c3ae75c2f2d195bb5750d40a824e7
GavinAnderson-g <gander59@students.kennesaw.edu>
2025-04-14 21:52:16 -0400

    Create FakeInfoDetector.py
    Added a basic input prompt for article or link

---
+++ FakeInfoDetector.py
@@ -0,0 +1,7 @@
+def main():
+    print("Welcome to the FakeInfoDetector!")
+    user_input = input("Please enter a URL or article text: ")
+    print("You entered:", user_input)
+
+if __name__ == "__main__":
+    main()
